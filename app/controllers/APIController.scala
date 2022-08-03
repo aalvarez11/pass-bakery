83e6a3823dd9beb3f024bc46b411547efea447c4
aalvarez11 <aalvarez1@email.arizona.edu>
2022-08-03 15:12:57 -0700

    set up json, have two time formats available

--- app/controllers/APIController.scala
+++ app/controllers/APIController.scala
@@ -1,8 +1,13 @@
 package controllers
 
+import java.time.LocalDateTime
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.Environment._
+
+import java.time.format.DateTimeFormatter
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -11,22 +16,23 @@ import play.api.mvc._
 @Singleton
 class APIController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+  /*
+   * method that returns a json object with the following information:
+   * { "service" : "pass-bakery"
+   *   "environment" : string (production or development run, based on play fw env config)
+   *   "serverTime" : string (local time from dateTime library in ISO 8601 format)
+   * */
   def getStatus() = Action { implicit request: Request[AnyContent] =>
-    Ok("you are getting a message")
-  }
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-//  def index() = Action { implicit request: Request[AnyContent] =>
-//    Ok(views.html.index())
-//  }
-//
-//  def hello() = Action { implicit request: Request[AnyContent] =>
-//    Ok(views.html.hello())
-//  }
+
+    // construct the json
+    val json: JsValue = Json.obj(
+      "service" -> "pass-bakery",
+      "environment" -> play.api.Environment.toString,
+      "serverTime" -> DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now()),
+      "fixTime" -> LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    )
+
+    Ok(json)
+  }
 }
