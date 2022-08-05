6aa5d720fb8dde54d1fa9368f546f4c7ba5433f1
aalvarez11 <aalvarez1@email.arizona.edu>
2022-08-05 08:45:30 -0700

    moved implicit to companion object

--- app/controllers/APIController.scala
+++ app/controllers/APIController.scala
@@ -24,7 +24,7 @@ class APIController @Inject()(val controllerComponents: ControllerComponents, en
    */
 
   def getStatus() = Action { implicit request: Request[AnyContent] =>
-    implicit val statusWrites: OWrites[StatusInfo] = Json.writes[StatusInfo]
+
     val userEnvironment = env.mode.toString
     val formattedTimestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
     val userInfo = StatusInfo("pass-bakery", userEnvironment, formattedTimestamp)
--- app/models/StatusInfo.scala
+++ app/models/StatusInfo.scala
@@ -1,3 +1,9 @@
 package models
 
+import play.api.libs.json._
+
 case class StatusInfo(service: String, environment: String, serverTime: String)
+
+object StatusInfo {
+  implicit val statusWrites: OWrites[StatusInfo] = Json.writes[StatusInfo]
+}
