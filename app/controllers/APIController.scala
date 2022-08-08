a68cc5e1bc8242137abda479a82446fd405b5348
Alejandro Alvarez <alejandro.alvarez@rallyhealth.com>
2022-08-08 16:02:31 -0700

    scalafmt fixing

--- app/controllers/APIController.scala
+++ app/controllers/APIController.scala
@@ -9,22 +9,22 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+/** This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class APIController @Inject()(val controllerComponents: ControllerComponents, statInfo: StatusInfoService) extends BaseController {
+class APIController @Inject() (
+    val controllerComponents: ControllerComponents,
+    statInfo: StatusInfoService
+) extends BaseController {
 
-  /**
-   * method that returns a json object with the following information:
-   * { "service" : "pass-bakery"
-   *   "environment" : string (production or development run, based on play fw env config)
-   *   "serverTime" : string (local time from dateTime library in ISO 8601 format)
-   * }
-   */
+  /** method that returns a json object with the following information:
+    * { "service" : "pass-bakery"
+    *   "environment" : string (production or development run, based on play fw env config)
+    *   "serverTime" : string (local time from dateTime library in ISO 8601 format)
+    * }
+    */
   def getStatus() = Action { implicit request: Request[AnyContent] =>
-
     Ok(statInfo.getUserStatus())
   }
 }
