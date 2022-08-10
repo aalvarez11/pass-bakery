54f9d7ff76e0b643de913fdbf6ad8699b0db8560
Alejandro Alvarez <alejandro.alvarez@rallyhealth.com>
2022-08-10 14:54:31 -0700

    trying a json return

--- app/DAO/BakeryDatabase.scala
+++ app/DAO/BakeryDatabase.scala
@@ -2,19 +2,30 @@ package DAO
 
 import javax.inject.Inject
 import play.api.db.Database
+
 import scala.concurrent.Future
 import models.DatabaseExecutionContext
+import play.api.libs.json._
 
 class BakeryDatabase @Inject() (
     db: Database,
     databaseExecutionContext: DatabaseExecutionContext
 ) {
-  def updateSomething(): Unit = {
+  def getDatabaseName(): Future[JsValue] = {
     Future {
       db.withConnection { conn =>
         // do something with your db
-        db.name
+        val dbName: String = db.name
+        Json.parse("""
+            {
+              "database name" : dbName
+            }
+            """)
       }
     }(databaseExecutionContext)
   }
+}
+
+object BakeryDatabase {
+  implicit val dbWrites: OWrites[Future[JsValue]] = Json.writes[Future[JsValue]]
 }
