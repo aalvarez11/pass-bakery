e752b3e9e3267429213c6c479de2722a9c499831
Alejandro Alvarez <alejandro.alvarez@rallyhealth.com>
2022-08-11 13:14:17 -0700

    removed character limit for 'name', added NOT NULL constraint & default current value for the timestamp columns

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,12 +3,12 @@
 -- !Ups
 
 CREATE TABLE IF NOT EXISTS Product (
-    id          UUID          NOT NULL PRIMARY KEY,
-    name        VARCHAR(100)  NOT NULL,
+    id          UUID        NOT NULL PRIMARY KEY,
+    name        VARCHAR     NOT NULL,
     quantity    INTEGER,
     price       DECIMAL,
-    created_at  TIMESTAMP,
-    updated_at  TIMESTAMP
+    created_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- !Downs
