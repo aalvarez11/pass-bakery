eecdca79a0d5b30c155f9add9c282d009bee43ff
Alejandro Alvarez <alejandro.alvarez@rallyhealth.com>
2022-08-11 13:15:24 -0700

    removed file and directory, unnecessary with evolutions

--- app/sql/bakeryDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-    id          UUID          NOT NULL PRIMARY KEY,
-    name        VARCHAR(100)  NOT NULL,
-    quantity    INTEGER,
-    price       DECIMAL,
-    created_at  TIMESTAMP,
-    updated_at  TIMESTAMP
-);
