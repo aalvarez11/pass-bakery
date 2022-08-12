f906b69c361964a3c167abfd3072c5fc9620198d
Alejandro Alvarez <alejandro.alvarez@rallyhealth.com>
2022-08-12 16:35:14 -0700

    added some functions, only getAllProducts works for now

--- app/DAO/BakeryDatabase.scala
+++ app/DAO/BakeryDatabase.scala
@@ -5,6 +5,7 @@ import play.api.db.Database
 
 import scala.concurrent.Future
 import models.DatabaseExecutionContext
+import play.api.libs.json.JsValue
 
 class BakeryDatabase @Inject() (
     db: Database,
@@ -14,7 +15,6 @@ class BakeryDatabase @Inject() (
   def getDatabaseTables: Future[String] = {
     Future {
       db.withConnection { conn =>
-        // do something with your db
         val result = conn.getMetaData.getTables(
           null,
           null,
@@ -32,6 +32,76 @@ class BakeryDatabase @Inject() (
           }
         }
         tableListStr
+      }
+    }
+  }
+
+  def addNewProduct(inJson: JsValue): Future[String] = {
+    Future {
+      db.withConnection { conn =>
+        val t = ""
+
+        try {
+
+          /** the following must be done to add a new record/row to a table:
+            *    1. establish a connection to the database
+            *    2. connect to the table that will be updated (should this be a const val within this class?)
+            *    3. parse json object for column values to be inserted in the new row
+            *    4. insert the values to the table
+            *    4. commit the transaction, rollback if you can't
+            */
+          val statement =
+            conn.prepareStatement("INSERT INTO product() VALUES ()")
+          val insertResult = statement.executeQuery()
+        } catch {
+          case e: Exception => e.printStackTrace().toString
+        }
+
+        t
+      }
+    }
+  }
+
+  def getAllProducts: Future[String] = {
+    Future {
+      db.withConnection { conn =>
+        try {
+          val statement = conn.prepareStatement("SELECT * FROM product;")
+          val selectResult = statement.executeQuery()
+          var returnStr = ""
+          var idx = 1;
+          while (selectResult.next()) {
+            returnStr += "Item " + idx + ":: id: " + selectResult.getString(
+              "id"
+            ) + ", name: " + selectResult.getString(
+              "name"
+            ) + ", quantity: " + selectResult.getInt(
+              "quantity"
+            ) + ", price: " + selectResult.getDouble(
+              "price"
+            ) + ", created at: " + selectResult.getTimestamp(
+              "created_at"
+            ) + ", updated at: " + selectResult.getTimestamp(
+              "updated_at"
+            ) + "\n"
+            idx += 1
+          }
+          returnStr
+        } catch {
+          case e: Exception => e.printStackTrace().toString
+        }
+      }
+    }
+  }
+
+  def getProductById: Future[String] = {
+    Future {
+      db.withConnection { conn =>
+        try {
+          val stmnt = conn.prepareStatement("SELECT ")
+        } catch {
+          case e: Exception => e.printStackTrace().toString
+        }
       }
     }
   }
